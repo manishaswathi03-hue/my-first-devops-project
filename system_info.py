5b0cc3adc701e9d4b5df6d474ac59dfe3a7b4782
manishaswathi03-hue <manishaswathi03@gmail.com>
2026-09-13 10:47:57 +0530

    Update system_info.py

--- system_info.py
+++ system_info.py
@@ -7,3 +7,12 @@ print("Operating System:", platform.system())
 print("OS Version:", platform.version())
 print("Python Version:", platform.python_version())
 print("Processor:", platform.processor())
+import socket
+import shutil
+
+print("IP Address:", socket.gethostbyname(socket.gethostname()))
+
+total, used, free = shutil.disk_usage("/")
+print("Total Disk Space:", total // (2**30), "GB")
+print("Used Disk Space:", used // (2**30), "GB")
+print("Free Disk Space:", free // (2**30), "GB")
